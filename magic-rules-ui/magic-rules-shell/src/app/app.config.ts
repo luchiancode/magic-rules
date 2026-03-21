b2b6f5fd46295a6c3e4cfeaf201825a998517a54
luchiancode <luchian.ionica@outlook.com>
2026-03-22 00:18:50 +0200

    use module federation

--- magic-rules-ui/magic-rules-shell/src/app/app.config.ts
+++ magic-rules-ui/magic-rules-shell/src/app/app.config.ts
@@ -1,11 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
 };
